1cd55039251804d706433f036b77fd1d2b37b609
WhoAbdullahSheikh <abdullahmuhhhamad339@gmail.com>
2024-04-26 03:46:55 +0500

    SQL Added

--- database/rehbar.sql
+++ database/rehbar.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2024 at 06:00 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.10
+-- Generation Time: Apr 26, 2024 at 12:46 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,44 +24,43 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `service_provider_users`
+-- Table structure for table `service_provider`
 --
 
-CREATE TABLE `service_provider_users` (
-  `Name` varchar(255) NOT NULL,
-  `Username` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL
+CREATE TABLE `service_provider` (
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `service_provider_users`
+-- Dumping data for table `service_provider`
 --
 
-INSERT INTO `service_provider_users` (`Name`, `Username`, `Password`) VALUES
-('Abdullah Sheikh', 'Coursehero256@gmail.com', '1234'),
-('Abdullah Sheikh', 'Coursehero256@gmail.com', '1234'),
-('Abdullah Sheikh', 'Coursehero256@gmail.com', '1234'),
-('Abdullah Sheikh', 'Coursehero256@gmail.com', '1234'),
-('Abdullah 122', 'bse203018@cust.pk', '1122');
+INSERT INTO `service_provider` (`fullname`, `email`, `password`) VALUES
+('shaaz', 'abdullah@gmail.com', '1234'),
+('Abdullah SHeikh Muhammad', 'sa8395714@gmail.com', '1234'),
+('AbdulRehman', 'ar@gmail.com', '13131'),
+('Daniyal', 'dkajsdlokj@outlook.com', '1234');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tourist_users`
+-- Table structure for table `tourist`
 --
 
-CREATE TABLE `tourist_users` (
-  `Name` varchar(255) NOT NULL,
-  `Username` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL
+CREATE TABLE `tourist` (
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tourist_users`
+-- Dumping data for table `tourist`
 --
 
-INSERT INTO `tourist_users` (`Name`, `Username`, `Password`) VALUES
-('SHEIKH RAHAT ALI hashim', 'abdullah@gmail.com', '1111');
+INSERT INTO `tourist` (`fullname`, `email`, `password`) VALUES
+('Daniyal', 'abdullahmuhhamad339@gmail.com', '1234');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
